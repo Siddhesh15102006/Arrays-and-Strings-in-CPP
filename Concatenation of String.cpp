22a3dcd714a65503a733c61858c349223159b068
Siddhesh Gajare <ssgajare12345@gmail.com>
2025-09-13 00:07:26 +0530

    Concatenation of String.cpp

---
+++ Concatenation of String.cpp
@@ -0,0 +1,32 @@
+// Siddhesh Gajare
+// 25070123505
+// A3
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+    string str1;
+    string str2;
+    
+    cout<<"Enter the First String :- ";
+    cin>>str1;
+    
+    cout<<"Enter the Second String :- ";
+    cin>>str2;
+    
+    string str;
+    str= str1+  " " + str2;
+    cout<<"The Concatenated String is :- "<<str;
+    
+    return 0;
+}
+
+Output :-
+Enter the First String :- Siddhesh
+Enter the Second String :- Gajare
+The Concatenated String is :- Siddhesh Gajare
+
+=== Code Execution Successful ===
