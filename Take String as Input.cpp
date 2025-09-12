82ecda2e5aa975e3adc1a33c108e27e875edddaa
Siddhesh Gajare <ssgajare12345@gmail.com>
2025-09-12 23:59:11 +0530

    Create Take String as Input.cpp

---
+++ Take String as Input.cpp
@@ -0,0 +1,23 @@
+// Siddhesh Gajare
+// 25070123505
+// A3
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str1;
+    
+    cout<<"Enter the String - ";
+    cin>>str1;
+    cout<<str1;
+    
+    return 0;
+}
+
+Output :-
+Enter the String - Siddhesh
+Siddhesh
+
+=== Code Execution Successful ===
